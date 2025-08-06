a13b868401e94c8f73c7be51eb22be4702ffbe15
Manish Choudhary <me.developer4u@gmail.com>
2025-08-06 13:00:51 +0530

    Update

---
+++ src/A_Time_and_Space_Complexity/a_SwapingTwoNumber.java
@@ -0,0 +1,19 @@
+package A_Time_and_Space_Complexity;
+
+public class a_SwapingTwoNumber {
+    public static void main(String [] args){
+        int a =10;     // 1 unit
+        int b =20;     // 1 unit
+        System.out.println("before swapping: "+"a :"+a+" b :"+b);   // 1 unit
+
+        int temp = a;   // 1 unit
+         a = b;        // 1 unit
+         b =temp;      // 1 unit
+        System.out.println("After swapping: "+"a :"+a+" b :"+b);       // 1 unit
+
+        // total unit = 7 unit
+        // contstant ignore it means == 1 unit
+        // f(n) = 1 unit time
+
+    }
+}
