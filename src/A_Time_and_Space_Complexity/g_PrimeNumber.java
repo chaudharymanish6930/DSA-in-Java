6a276316d187bebcee4daecba004637750b7b5a2
Manish Choudhary <me.developer4u@gmail.com>
2025-08-06 20:26:47 +0530

    Update

---
+++ src/A_Time_and_Space_Complexity/g_PrimeNumber.java
@@ -0,0 +1,32 @@
+package A_Time_and_Space_Complexity;
+import java.util.Scanner;
+public class g_PrimeNumber {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter a number: ");
+        int num = sc.nextInt();
+
+        boolean isPrime = true;
+
+        if (num <= 1) {
+            isPrime = false;
+        } else {
+            // Check from 2 to sqrt(num)
+            for (int i = 2; i <= Math.sqrt(num); i++) {
+                if (num % i == 0) {
+                    isPrime = false;
+                    break;
+                }
+            }
+        }
+
+        if (isPrime) {
+            System.out.println(num + " is a Prime number.");
+        } else {
+            System.out.println(num + " is NOT a Prime number.");
+        }
+
+        sc.close();
+    }
+}
