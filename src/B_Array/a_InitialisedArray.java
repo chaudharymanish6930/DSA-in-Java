f88f507324636274d0ec30d2072b63b44c655135
Manish Choudhary <me.developer4u@gmail.com>
2025-08-07 11:36:37 +0530

    update

---
+++ src/B_Array/a_InitialisedArray.java
@@ -0,0 +1,13 @@
+package B_Array;
+
+public class a_InitialisedArray {
+    public static void main(String[] args){
+        int[]  arr ={101,102,103,104,105,106};
+
+        System.out.println(arr[3]);
+
+        System.out.println(arr.length);
+
+        System.out.println(arr[arr.length-1]);
+    }
+}
