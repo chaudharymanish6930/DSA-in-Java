317244120df2a66bb9c30c657dcfaefcb6c4eccb
Manish Choudhary <me.developer4u@gmail.com>
2025-08-07 23:05:01 +0530

    Update

---
+++ src/C_Array_Operations/C_Sorting_Array/a_BubbleSorting.java
@@ -0,0 +1,35 @@
+package C_Array_Operations.C_Sorting_Array;
+
+public class a_BubbleSorting {
+    public static void main(String[] args){
+        int [] arr = {12,32,11,16,9,980,76,57};
+
+        System.out.println("Before sorting: ");
+        for(int no:arr){
+            System.out.print(no+" ");
+        }
+        System.out.println();
+        int leng = arr.length;
+
+        int temp;
+        for(int i=1; i<leng; i++){
+            boolean swapped = false;
+            for(int j=0; j<leng-i; j++){  // 1 -> i
+                if(arr[j] > arr[j+1]){
+                    temp = arr[j];
+                    arr[j] = arr[j+1];
+                    arr[j+1] = temp;
+                    swapped = true;
+                }
+            }
+            if(swapped== false){
+                break;
+            }
+        }
+
+        System.out.println("after sorting: ");
+        for(int i=0; i<leng; i++){
+            System.out.print(arr[i]+" ");
+        }
+    }
+}
