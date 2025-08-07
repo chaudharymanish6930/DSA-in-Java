e5c9aa02b4afd36d6b5db58b004387ae09379d54
Manish Choudhary <me.developer4u@gmail.com>
2025-08-07 12:06:42 +0530

    Update

---
+++ src/B_Array/d_ArrayDeclaration.java
@@ -0,0 +1,12 @@
+package B_Array;
+
+public class d_ArrayDeclaration {
+    public static void main(String[] args){
+        int nums[]={1,2,3,4,5};
+        System.out.println(nums[2]);
+
+        int array[]=new int[5];
+        array[3]=6;
+        System.out.println(array[3]);
+    }
+}
