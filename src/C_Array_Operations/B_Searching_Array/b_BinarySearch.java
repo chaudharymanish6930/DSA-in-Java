fcef148c77fcb7c79b63b1f9437494693e54f271
Manish Choudhary <me.developer4u@gmail.com>
2025-08-07 16:34:48 +0530

    Update

---
+++ src/C_Array_Operations/B_Searching_Array/b_BinarySearch.java
@@ -0,0 +1,29 @@
+package C_Array_Operations.B_Searching_Array;
+
+public class b_BinarySearch {
+    public static void main(String[] args) {
+        int[] arr = {2, 4, 6, 8, 10, 12, 14};  // Predefined sorted array
+        int target = 10;                      // Element to search
+
+        int start = 0, end = arr.length - 1;
+        boolean found = false;
+
+        while (start <= end) {
+            int mid = (start + end) / 2;
+
+            if (arr[mid] == target) {
+                System.out.println("Element " + target + " found at index " + mid);
+                found = true;
+                break;
+            } else if (arr[mid] < target) {
+                start = mid + 1;
+            } else {
+                end = mid - 1;
+            }
+        }
+
+        if (!found) {
+            System.out.println("Element " + target + " not found.");
+        }
+    }
+}
