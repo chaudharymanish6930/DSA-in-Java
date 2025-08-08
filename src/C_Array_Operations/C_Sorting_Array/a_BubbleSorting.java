65f12e217db1e175ae88096615f4208cac06ef83
Manish Choudhary <me.developer4u@gmail.com>
2025-08-08 10:38:55 +0530

    Update

--- src/C_Array_Operations/C_Sorting_Array/a_BubbleSorting.java
+++ src/C_Array_Operations/C_Sorting_Array/a_BubbleSorting.java
@@ -12,8 +12,8 @@ public class a_BubbleSorting {
         int leng = arr.length;
 
         int temp;
-        for(int i=1; i<=leng; i++){
-            for(int j=0; j<leng-i; j++){  // 1 -> i
+        for(int i=0; i<=leng-1; i++){
+            for(int j=0; j<leng-i-1; j++){  // 1 -> i
                 if(arr[j] > arr[j+1]){
                     temp = arr[j];
                     arr[j] = arr[j+1];
