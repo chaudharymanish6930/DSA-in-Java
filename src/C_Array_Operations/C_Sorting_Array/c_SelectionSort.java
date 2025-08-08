1742d00744d8dfc54e8bf11bfaf4e4c24830bc2c
Manish Choudhary <me.developer4u@gmail.com>
2025-08-08 16:42:48 +0530

    Update

---
+++ src/C_Array_Operations/C_Sorting_Array/c_SelectionSort.java
@@ -0,0 +1,29 @@
+package C_Array_Operations.C_Sorting_Array;
+
+public class c_SelectionSort {
+    public static void main(String[] args) {
+        int[] arr = {10, 13, 54, 23, 9, 32};
+        int size = arr.length;
+
+        for (int i = 0; i < size - 1; i++) {
+            int minIndex = i;
+
+            for (int j = i + 1; j < size; j++) {
+                if (arr[j] < arr[minIndex]) {
+                    minIndex = j;
+                }
+            }
+            // swapping...
+            int temp = arr[minIndex];
+            arr[minIndex] = arr[i];
+            arr[i] = temp;
+            System.out.println(arr[i]+" ");
+        }
+
+        System.out.println();
+        for (int n : arr) {
+            System.out.print(n + " ");
+
+        }
+    }
+}
