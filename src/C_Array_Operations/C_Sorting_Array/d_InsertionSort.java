c9625757ed39070cb9bac655bdd7207ada8db1aa
Manish Choudhary <me.developer4u@gmail.com>
2025-08-10 10:24:21 +0530

    Update

---
+++ src/C_Array_Operations/C_Sorting_Array/d_InsertionSort.java
@@ -0,0 +1,24 @@
+package C_Array_Operations.C_Sorting_Array;
+
+public class d_InsertionSort {
+    public static void main(String [] args){
+        int[] arr = {5, 1, 6, 2, 4, 3};
+        int temp;
+        int j;
+
+        for (int i = 1; i < arr.length; i++) {
+            temp = arr[i];
+            j = i;
+            while (j > 0 && arr[j - 1] > temp) {
+                arr[j] = arr[j - 1];
+                j = j - 1;
+            }
+            arr[j] = temp; // insert temp in the correct position
+        }
+
+        // Print sorted array
+        for (int num : arr) {
+            System.out.print(num + " ");
+        }
+    }
+}
