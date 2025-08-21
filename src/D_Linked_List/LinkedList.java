c4c2c669bb01a165436741ca7119c4daca31177f
Manish Choudhary <me.developer4u@gmail.com>
2025-08-21 13:29:17 +0530

    Update

--- src/D_Linked_List/LinkedList.java
+++ src/D_Linked_List/LinkedList.java
@@ -3,9 +3,43 @@ package D_Linked_List;
 class node{
     int data;
     node next;
-}
-public class LinkedList {
-    public void add(int data){
 
+    public node(int data){
+        this.data=data;
+        this.next=null;
+    }
+}
+
+
+public class LinkedList {
+    node head;
+
+    public void add(int data){
+        node newNode = new node(data);
+
+        node currNode = head;
+        if(head == null){
+            head = newNode;
+        }
+        else{
+            while(currNode.next != null){
+                currNode = currNode.next;
+            }
+            currNode.next = newNode;
+        }
+    }
+
+    public void printvalue(){
+        if(head== null){
+            System.out.println("list is empty");
+            return;
+        }
+
+        node currNode = head;
+        while(currNode != null){
+            System.out.print(currNode.data+"-> ");
+            currNode = currNode.next;
+        }
+        System.out.println("Null");
     }
 }
--- src/D_Linked_List/a_FirstLinkedList.java
+++ src/D_Linked_List/a_FirstLinkedList.java
@@ -1,15 +1,11 @@
 package D_Linked_List;
 
-//import java.util.LinkedList;
-
-//import java.util.LinkedList;
-
 public class a_FirstLinkedList {
     public  static void main(String[] args){
         LinkedList num = new LinkedList();
 
         num.add(4);
-        num.add("manish");
-        System.out.println(link);
+        num.add(6);
+        num.printvalue();
     }
 }
