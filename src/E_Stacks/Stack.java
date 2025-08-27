5c8aa3212138da53896b811876f9993ee15b294f
Manish Choudhary <me.developer4u@gmail.com>
2025-08-27 15:36:51 +0530

    Update

---
+++ src/E_Stacks/Stack.java
@@ -0,0 +1,62 @@
+package E_Stacks;
+
+public class Stack {
+    private int maxSize;   // maximum size of stack
+    private int[] stack;   // array to store elements
+    private int top;       // index of top element
+
+    public  Stack(int size){
+        maxSize = size;
+        stack = new int[maxSize];
+        top = -1;
+    }
+
+    public void push(int data){
+        if(isFull()){
+            System.out.println("stack is overflow! can't push"+data);
+        }
+        else {
+            stack[++top] = data;
+            System.out.println(data+" pushed into stack");
+        }
+    }
+
+    public void pop(){
+        if(isEmpty()){
+            System.out.println("Stack underflow! cannot pop");
+        }else{
+            System.out.println(stack[top--]+ " popped from stack");
+        }
+    }
+
+    public void peek(){
+        if(isEmpty()){
+            System.out.println("Stack is empty");
+        }
+        else{
+            System.out.println("Top element: "+stack[top]);
+        }
+    }
+
+    public boolean isEmpty(){
+        return (top == -1);
+    }
+
+    public boolean isFull(){
+        return (top == maxSize-1);
+    }
+
+    public void display(){
+        if(isEmpty()){
+            System.out.println("Stack is empty");
+        }
+        else{
+            System.out.println("Stack elemnts: ");
+            for(int i=0; i<=top; i++){
+                System.out.print(stack[i]+" ");
+            }
+            System.out.println( );
+        }
+    }
+
+}
---
+++ src/E_Stacks/b_usingArray.java
@@ -0,0 +1,49 @@
+package E_Stacks;
+
+import java.util.*;
+
+public class b_usingArray {
+
+    public static void main(String[] args){
+    Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Enter the size of stack: ");
+        int size = scanner.nextInt();
+
+        Stack stack = new Stack(size);
+
+        while(true){
+            System.out.println("\n----Stack menu----");
+            System.out.println("1. Push");
+            System.out.println("2. Pop");
+            System.out.println("3. Peek");
+            System.out.println("4. Display");
+            System.out.println("5. Exit");
+            System.out.print("Enter your choice:");
+            int choice = scanner.nextInt();
+
+            switch(choice){
+                case 1:
+                    System.out.print("enter the data : ");
+                    int data = scanner.nextInt();
+                    stack.push(data);
+                    break;
+                case 2:
+                    stack.pop();
+                    break;
+                case 3:
+                    stack.peek();
+                    break;
+                case 4:
+                    stack.display();
+                    break;
+                case 5:
+                    System.out.print("exiting....");
+                    scanner.close();
+                    return;
+                default:
+                    System.out.print("invalid choice");
+            }
+        }
+    }
+}
