c4404726e478099f24648f0b840e0a664033337d
Manish Choudhary <me.developer4u@gmail.com>
2025-08-31 22:51:18 +0530

    Update

--- src/F_Queue/A_Collection_FrameWork/a_Basics.java
+++ src/F_Queue/A_Collection_FrameWork/a_Basics.java
@@ -4,96 +4,12 @@ import java.util.*;
 
 public class a_Basics {
     public static void main(String[] args) {
-        Queue<Integer> queue = new Queue<Integer>() {
-            @Override
-            public boolean add(Integer integer) {
-                return false;
-            }
+        Queue<Integer> queue = new LinkedList<>();
 
-            @Override
-            public boolean offer(Integer integer) {
-                return false;
-            }
+        queue.add(10);
+        queue.add(20);
+        queue.add(30);
+        System.out.println(queue);
 
-            @Override
-            public Integer remove() {
-                return 0;
-            }
-
-            @Override
-            public Integer poll() {
-                return 0;
-            }
-
-            @Override
-            public Integer element() {
-                return 0;
-            }
-
-            @Override
-            public Integer peek() {
-                return 0;
-            }
-
-            @Override
-            public int size() {
-                return 0;
-            }
-
-            @Override
-            public boolean isEmpty() {
-                return false;
-            }
-
-            @Override
-            public boolean contains(Object o) {
-                return false;
-            }
-
-            @Override
-            public Iterator<Integer> iterator() {
-                return null;
-            }
-
-            @Override
-            public Object[] toArray() {
-                return new Object[0];
-            }
-
-            @Override
-            public <T> T[] toArray(T[] a) {
-                return null;
-            }
-
-            @Override
-            public boolean remove(Object o) {
-                return false;
-            }
-
-            @Override
-            public boolean containsAll(Collection<?> c) {
-                return false;
-            }
-
-            @Override
-            public boolean addAll(Collection<? extends Integer> c) {
-                return false;
-            }
-
-            @Override
-            public boolean removeAll(Collection<?> c) {
-                return false;
-            }
-
-            @Override
-            public boolean retainAll(Collection<?> c) {
-                return false;
-            }
-
-            @Override
-            public void clear() {
-
-            }
-        }
     }
 }
