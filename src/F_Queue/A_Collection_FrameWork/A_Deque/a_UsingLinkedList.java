83408be20132a22f2b75ba9d36be960b7f340b9a
Manish Choudhary <me.developer4u@gmail.com>
2025-08-31 23:23:02 +0530

    Update

--- src/F_Queue/A_Collection_FrameWork/A_Deque/a_UsingLinkedList.java
+++ src/F_Queue/A_Collection_FrameWork/A_Deque/a_UsingLinkedList.java
@@ -7,9 +7,7 @@ public class a_UsingLinkedList {
         // Create a Queue using LinkedList
         Queue<Integer> queue = new LinkedList<>();
 
-        // ---------------------------
         // 1. Enqueue (add / offer)
-        // ---------------------------
         queue.add(10);   // throws exception if capacity full
         queue.add(20);
         queue.offer(30); // preferred (returns false if fails instead of exception)
@@ -17,38 +15,28 @@ public class a_UsingLinkedList {
 
         System.out.println("Queue after enqueues: " + queue);
 
-        // ---------------------------
         // 2. Peek methods
-        // ---------------------------
-        System.out.println("Peek (element): " + queue.element()); // head, exception if empty
-        System.out.println("Peek (peek): " + queue.peek());       // head, null if empty
+        System.out.println("Peek (element): " + queue.element()); // front, exception if empty
+        System.out.println("Peek (peek): " + queue.peek());       // front, null if empty
 
-        // ---------------------------
         // 3. Dequeue (remove / poll)
-        // ---------------------------
         System.out.println("Removed (remove): " + queue.remove()); // removes head, exception if empty
         System.out.println("Removed (poll): " + queue.poll());     // removes head, null if empty
         System.out.println("Queue after removals: " + queue);
 
-        // ---------------------------
         // 4. Size and checks
-        // ---------------------------
         System.out.println("Queue size: " + queue.size());
         System.out.println("Is Queue empty? " + queue.isEmpty());
         System.out.println("Does Queue contain 20? " + queue.contains(20));
 
-        // ---------------------------
         // 5. Iterate through Queue
-        // ---------------------------
         System.out.print("Iterating Queue: ");
         for (Integer num : queue) {
             System.out.print(num + " ");
         }
         System.out.println();
 
-        // ---------------------------
         // 6. Clear Queue
-        // ---------------------------
         queue.clear();
         System.out.println("Queue after clear: " + queue);
         System.out.println("Is Queue empty now? " + queue.isEmpty());
