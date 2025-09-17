49888cb4a9b398b9f0e389b4f2aa52af54a53b31
Manish Choudhary <me.developer4u@gmail.com>
2025-09-17 10:38:35 +0530

    Update

--- src/F_Queue/D_Circular_Queue/a_ImplementationInArray.java
+++ src/F_Queue/D_Circular_Queue/a_ImplementationInArray.java
@@ -1,7 +1,80 @@
 package F_Queue.D_Circular_Queue;
 
 public class a_ImplementationInArray {
+    public static class QueueArray{
+        int front =-1;
+        int rear = -1;
+        int[] arr = new int[5];
+        int size =0;
+
+        public void add(int data){
+            if(size==0){
+                front = rear =0;
+                arr[0]=data;
+            }
+            else if (size == arr.length) {
+                System.out.println("queue is empty");
+            }
+            else if(rear < arr.length-1){
+                arr[++rear] =data;
+            }
+            else if(rear == arr.length-1){
+                rear =0;
+                arr[0]=data;
+            }
+            size++;
+        }
+
+        public int remove(){
+            if(size ==0){
+                System.out.println("Queue is empty");
+                size--;
+                return -1;
+            }
+            else if(front == arr.length-1){
+                int val = arr[front];
+                front =0;
+                size--;
+                return val;
+            }
+            else{
+                int val = arr[front];
+                front++;
+                size--;
+                return val;
+            }
+
+        }
+
+        public int  peek(){
+            if(size==0) return -1;
+            return arr[rear];
+        }
+
+        public void Display(){
+            if(size==0){
+                System.out.println("Queue is empty!!");
+            }
+            else if(front<=rear){
+                for(int i=front; i<=rear; i++){
+                    System.out.println(arr[i]+" ");
+                }
+            }
+            else{ // rear < front
+                for(int i=front; i<=arr.length; i++){
+                    System.out.println(arr[i]+" ");
+                }
+                for(int i=0; i<=rear; i++){
+                    System.out.println(arr[i]+" ");
+                }
+            }
+            System.out.println();
+        }
+    }
     public static void main(String[] args) {
-        System.out.println("hello World!");
+        QueueArray queue = new QueueArray();
+        queue.Display();
+        queue.add(10);
+        queue.Display();
     }
 }
