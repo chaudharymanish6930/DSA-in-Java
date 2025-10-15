06b15403b1edbf283a4bcc72eded98a5c4dc6149
Manish Choudhary <me.developer4u@gmail.com>
2025-10-15 22:22:23 +0530

    Update

---
+++ src/D_Linked_List/A_Singly_Linked_List/f_ReverseLinkedList.java
@@ -0,0 +1,36 @@
+package D_Linked_List.A_Singly_Linked_List;
+
+class ListNode {
+    int val;
+    ListNode next;
+
+    ListNode(int val) {
+        this.val = val;
+        this.next = null;
+    }
+}
+
+public class f_ReverseLinkedList {
+    public ListNode reverseList(ListNode head) {
+        // Initialize previous pointer as null
+        ListNode prev = null;
+        ListNode current = head;
+
+        // Traverse the list
+        while (current != null) {
+            // Temporarily store the next node
+            ListNode nextNode = current.next;
+
+            // Reverse the link
+            current.next = prev;
+
+            // Move pointers one position forward
+            prev = current;
+            current = nextNode;
+        }
+
+        // prev will be the new head
+        return prev;
+    }
+
+}
