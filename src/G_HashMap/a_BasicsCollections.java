15e703f086b26889fec97c7cb7ff97faa9e05810
Manish Choudhary <me.developer4u@gmail.com>
2025-10-23 17:33:08 +0530

    Update

--- src/G_HashMap/a_BasicsCollections.java
+++ src/G_HashMap/a_BasicsCollections.java
@@ -7,7 +7,7 @@ public class a_BasicsCollections {
     public static void main(String[] args) {
         Map<Integer, String> map = new HashMap<>();
 
-        // Adding elements
+        // Adding elements  put-> (key, value)
         map.put(1, "Apple");
         map.put(2, "Banana");
         map.put(3, "Cherry");
---
+++ src/G_HashMap/b_FrequentElemnetWithoutHashmap.java
@@ -0,0 +1,63 @@
+package G_HashMap;
+
+import java.util.Scanner;
+
+public class b_FrequentElemnetWithoutHashmap {
+    public static void main(String[] args) {
+        Scanner sc= new Scanner(System.in);
+        System.out.print("enter the length of the array: ");
+        int length = sc.nextInt();
+
+        int [] arr = new int[length];
+
+        for(int i=0; i<length; i++){
+            System.out.print("Enter the elment at index of "+i+" ");
+             arr[i]= sc.nextInt();
+        }
+
+        System.out.println("The given Array :");
+        for(int i=0; i<length; i++){
+            System.out.print(arr[i]+" ");
+        }
+
+        // sort the given data according to be used;
+        int temp;
+        for(int i=0; i<length-1; i++){
+            for(int j=0; j<length-i-1; j++){
+                if(arr[j]>arr[j+1]){
+                    temp= arr[j];
+                    arr[j]=arr[j+1];
+                    arr[j+1]=temp;
+                }
+            }
+        }
+        System.out.println("The sorted Array of the Element :");
+        for(int i:arr){
+            System.out.print(i+" ");
+        }
+        // both does same work in this code
+        //  for(int i=0; i<length; i++){
+        //      System.out.print(arr[i]+" ");
+        //  }
+
+        int maxfreq=1;
+        int current = 1;
+        int element = arr[0];
+        for(int i=1; i<length; i++){
+            if(arr[i]==arr[i-1]){
+                current++;
+            }
+            else{
+                current =1;
+            }
+
+            if(maxfreq<current){
+                maxfreq= current;
+                element = arr[i];
+            }
+        }
+
+        System.out.println("\nMost Frequent Element: " + element);
+        System.out.println("Frequency: " + maxfreq);
+    }
+}
