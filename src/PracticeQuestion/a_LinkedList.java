fb08d885ae1f0050fccd546c3dde9c1eff7a17f5
Manish Choudhary <me.developer4u@gmail.com>
2025-11-05 10:47:04 +0530

    Update

--- src/PracticeQuestion/a_LinkedList.java
+++ src/PracticeQuestion/a_LinkedList.java
@@ -9,7 +9,90 @@ class node{
     }
 }
 
-class
+class SinglyLinkedList{
+    node head;
+    public void add(int data){
+        node NewNode = new node(data);
+        node temp=head;
+        if(head== null){
+            head = NewNode;
+            return;
+        }
+        while(temp.next != null){
+            temp = temp.next;
+        }
+        temp.next = NewNode;
+        return;
+    }
+
+    public void addFirst(int data){
+        node NewNode = new node(data);
+        if(head==null){
+            head = NewNode;
+            return;
+        }
+        NewNode.next = head;
+        head = NewNode;
+    }
+
+    public void addLast(int data){
+        node Newnode = new node(data);
+        if(head==null){
+            head = Newnode;
+            return;
+        }
+
+        node temp = head;
+        while(temp.next!=null){
+            temp= temp.next;
+        }
+        temp.next = Newnode;
+    }
+
+    public int getSize(){
+        int i =0;
+        if(head==null){
+            System.out.println("List is empty");
+            return 1;
+        }
+        node temp = head;
+        while(temp.next==null){
+            i++;
+            temp = temp.next;
+        }
+        return i;
+    }
+
+    public void deleteFirst(){
+        if(head==null){
+            System.out.println("Your list is empty!!");
+        }
+        head = head.next;;
+    }
+
+    public void deleteLast(){
+        if(head==null){
+            System.out.println("Your list is Empty");
+            return;
+        }
+        if(head.next==null){
+            head=null;
+            return;
+        }
+        node temp = head;
+        while(temp.next.next != null){
+            temp = temp.next;
+        }
+        temp.next=null;
+    }
+
+    public void deleteAt(int index){
+        if(index<=0){
+            System.out.println("List is empty!!");
+        }
+
+    }
+}
 public class a_LinkedList {
     public static void main(String[] args) {
 
