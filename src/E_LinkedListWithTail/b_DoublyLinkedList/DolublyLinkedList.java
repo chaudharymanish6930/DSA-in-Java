f325bbde3c26be4c5bd261fed68db639fbf163df
Manish Choudhary <me.developer4u@gmail.com>
2025-11-09 23:38:13 +0530

    Update

--- src/E_LinkedListWithTail/b_DoublyLinkedList/DolublyLinkedList.java
+++ src/E_LinkedListWithTail/b_DoublyLinkedList/DolublyLinkedList.java
@@ -1,6 +1,18 @@
 package E_LinkedListWithTail.b_DoublyLinkedList;
 
 class node{
+    int data;
+    node prev;
+    node next;
+
+    node(int data){
+        this.data=data;
+        this.prev=null;
+        this.next=null;
+    }
+}
+
+class DoubleLinkedList{
 
 }
 public class DolublyLinkedList {
